74913e8f9487aa9f8e0528e792feeb4246dcaaa5
Mtjds <58536876+Mtjds@users.noreply.github.com>
2024-11-23 15:17:39 +0800

    Add files via upload

---
+++ DMRec-main/utils.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : utils.py
+# @Author:
+# @Date  : 2021/10/23 11:26
+# @Desc  :
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BPRLoss(nn.Module):
+    def __init__(self):
+        super(BPRLoss, self).__init__()
+        self.gamma = 1e-10
+
+    def forward(self, p_score, n_score):
+        loss = -torch.log(self.gamma + torch.sigmoid(p_score - n_score))
+        loss = loss.mean()
+
+        return loss
+
+
+class EmbLoss(nn.Module):
+    """ EmbLoss, regularization on embeddings
+
+    """
+
+    def __init__(self, norm=2):
+        super(EmbLoss, self).__init__()
+        self.norm = norm
+
+    def forward(self, *embeddings):
+        emb_loss = 0
+        for embedding in embeddings:
+            tmp = torch.norm(embedding, p=self.norm)
+            tmp = tmp / embedding.shape[0]
+            emb_loss += tmp
+        return emb_loss
+
+def InfoNCE(view1, view2, temperature: float, b_cos: bool = True):
+    """
+    Args:
+        view1: (torch.Tensor - N x D)
+        view2: (torch.Tensor - N x D)
+        temperature: float
+        b_cos (bool)
+
+    Return: Average InfoNCE Loss
+    """
+    if b_cos:
+        view1, view2 = F.normalize(view1, dim=1), F.normalize(view2, dim=1)
+
+    pos_score = (view1 @ view2.T) / temperature
+    score = torch.diag(F.log_softmax(pos_score, dim=1))
+    return -score.mean()
